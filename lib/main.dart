f096f39dcd4c6333bab33261af027b2c9ac9f22c
ds <ds@labdc.etec>
2026-04-22 19:37:43 -0300

    Versao Atualizada com Tutorial 5

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String? opcaoPagamento;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Quizz"), backgroundColor: Colors.green),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(20.0),
+
+          child:Column(
+            children: [
+              DropdownMenu<String>(
+                initialSelection: "Pix",
+                label: const Text("Forma de Pagamento"),
+                dropdownMenuEntries: const [
+                  DropdownMenuEntry(value: 'Pix', label: 'Pix' ),
+                  DropdownMenuEntry(value: 'Boleto', label: 'Boleto Bancário' ),
+                  DropdownMenuEntry(value: 'Cartão', label: 'Cartão de Crédito' ),
+                ],
+                onSelected: (String? novoValor) {
+                  print('Selecionado: $novoValor');
+                  opcaoPagamento = novoValor;
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                   print("Valor selecionado: ${opcaoPagamento}");
+                },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 100),
+              ),
+              child: Text("Go!"),
+          ),
+            ]
+          )
+        ),
+      ),
+    );
+  }
+}
